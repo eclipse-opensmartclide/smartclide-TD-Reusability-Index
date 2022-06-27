e2e44a89c4666af330d2c50fa9703ff022351451
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 17:26:35 +0200

    Revert "Added missing copyrights and reformated some others"
    This reverts commit f75b1e13eab013c693165468aff6c9dc110b6260.

--- src/main/java/gr/zisis/reusabilityapi/service/ReusabilityServiceBean.java
+++ src/main/java/gr/zisis/reusabilityapi/service/ReusabilityServiceBean.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (C) 2021-2022 UoM - University of Macedonia
- * 
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
- * SPDX-License-Identifier: EPL-2.0
- ******************************************************************************/
 package gr.zisis.reusabilityapi.service;
 
 import gr.zisis.reusabilityapi.controller.response.entity.*;
@@ -19,6 +10,17 @@ import java.util.*;
 
 import static java.lang.Math.log10;
 
+/*
+ *
+ *  * Copyright (C) 2021 UoM - University of Macedonia
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License 2.0 which is available at
+ *  * https://www.eclipse.org/legal/epl-2.0/
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *
+ */
 
 @Service
 public class ReusabilityServiceBean implements ReusabilityService {
